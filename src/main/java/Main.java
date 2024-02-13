59c45a4ce768c780688eae2185a7cdd2928e2cc1
GSaphal <gsaphal99@gmail.com>
2024-02-13 10:01:48 -0500

    (feat) added basic data models

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import Controller.GameEngineController;
+
+public class Main {
+    public static void main(String[] args) {
+        GameEngineController engine=new GameEngineController();
+    }
+}
