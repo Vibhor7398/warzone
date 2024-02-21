e73df0bb050177325e36d22b44c5951d86d28cba
Mohammad Zaid Shaikh <md.zaidshaikh2000@gmail.com>
2024-02-21 02:05:28 -0500

    updated test suites and added relevant comments for Reinforcement

---
+++ src/test/java/TestSuites/ControllerTestSuit.java
@@ -0,0 +1,18 @@
+/**
+ * @author Vibhor Gulati, Apoorva Sharma, Saphal Ghirmire, Inderjeet Singh Chauhan, Mohammad Zaid
+ * @version 1.0
+ */
+package TestSuites;
+
+import Controller.TestMapsController;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * This class represents a test suite for testing controller.
+ * It includes test cases for TestMapsController.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TestMapsController.class})
+public class ControllerTestSuit {
+}
