ecb3caf80fa967375d638587b104062750110131
Apoorva Sharma <apoorvasharma@apoorvas-mbp.wireless.concordia.ca>
2024-03-12 15:46:35 -0400

    Adds execute method

--- src/main/java/Orders/Advance.java
+++ src/main/java/Orders/Advance.java
@@ -8,6 +8,7 @@ public class Advance implements Order{
     int d_advance_armies;
     Country d_source_country;
     Country d_target_country;
+    boolean d_attack_successful;
 
     public Advance(Player p_player, Country p_country_source, Country p_country_target, int p_advance_num) {
         this.d_player = p_player;
@@ -41,6 +42,41 @@ public class Advance implements Order{
 
     @Override
     public void execute() {
+        if (!isValid()) {
+            System.out.println("Army cannot be advanced.");
+            return;
+        }
+        // target country belongs to current player, move the armies to the target country
+        if (d_player.getCountriesOwned().contains(d_target_country)) {
+            // Removed from source
+            d_source_country.setArmies(d_source_country.getArmies() - d_advance_armies);
+            // Added to target
+            d_target_country.setArmies(d_advance_armies + d_target_country.getArmies());
+        } else {
+            // target country belongs to another player, initiate an attack
+            int l_defenderArmies = d_target_country.getArmies();
+            int l_attackerArmies = d_advance_armies;
+
+            if (l_attackerArmies > l_defenderArmies) {
+                // Attacker wins, move armies to the target territory
+                // Removed from source
+                d_source_country.setArmies(d_source_country.getArmies() - d_advance_armies);
+
+                d_target_country.setArmies(l_attackerArmies - l_defenderArmies);
+                // todo
+                d_player.addCountryToCountriesOwned(d_target_country);
+
+                Player l_target_player = d_target_country.getOwner();
+                l_target_player.removeCountryFromCountriesOwned(d_target_country);
+                d_attack_successful = true;
+
+            } else {
+                // Defender wins, update armies in source territory
+                d_source_country.setArmies(d_source_country.getArmies() - d_advance_armies);
+                d_attack_successful = false;
+            }
+        }
+        print();
 
     }
 
