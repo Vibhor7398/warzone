20540e3621559b130f7e79069660b9af00c2a3a3
Apoorva Sharma <apoorvasharma@apoorvas-mbp.wireless.concordia.ca>
2024-03-17 19:55:27 -0400

    Adds javadoc in CardAssignment service

--- src/main/java/Services/CardAssignment.java
+++ src/main/java/Services/CardAssignment.java
@@ -1,8 +1,15 @@
 package Services;
 
 import java.util.Random;
-
+/**
+ * This class provides methods for assigning random cards to players.
+ */
 public class CardAssignment {
+    /**
+     * Retrieves a random card from the available card pool.
+     *
+     * @return A randomly selected card.
+     */
     public static String getCard(){
         String[] l_cards = {"Bomb", "Blockade", "Airlift", "Negotiate"};
         Random l_rand = new Random();
