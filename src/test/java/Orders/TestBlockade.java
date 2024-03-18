d63d6ceb705cd5efabac9903057c55e068936497
Mohammad Zaid Shaikh <md.zaidshaikh2000@gmail.com>
2024-03-17 20:29:44 -0400

    Added Test Cases for Blockade and Diplomacy

--- src/test/java/Orders/TestBlockade.java
+++ src/test/java/Orders/TestBlockade.java
@@ -1,80 +1,78 @@
-//package Orders;
-//
-//import static org.junit.Assert.*;
-//
-//import Controller.GameEngineController;
-//import org.junit.Before;
-//import org.junit.Test;
-//import java.util.ArrayList;
-//import java.util.List;
-//import Models.Country;
-//import Models.Player;
-//
-//public class TestBlockade {
-//    private Player d_player;
-//    private Country d_country;
-//
-//    @Before
-//    public void setUp() {
-//        GameEngineController gameEngineController = new GameEngineController();
-//        gameEngineController.executeAddGamePlayer("TestPlayer");
-////        Phases
-//        d_player = new Player("TestPlayer");
-//        d_country = new Country("TestCountry");
-//        d_player.addCountryToCountriesOwned(d_country);
-//
-//        d_player = new Player("Abhi");
-//        d_player.addCard("Bloackade");
-//    }
-//
-//    @Test
-//    public void testIsValidWithNullPlayer() {
-//        Blockade blockade = new Blockade(null, d_country);
-//        assertFalse(blockade.isValid());
-//    }
-//
-//    @Test
-//    public void testIsValidWithNullCountry() {
-//        Blockade blockade = new Blockade(d_player, null);
-//        assertFalse(blockade.isValid());
-//    }
-//
-//    @Test
-//    public void testIsValidWithNoBlockadeCard() {
-//        Blockade blockade = new Blockade(d_player, d_country);
-//        assertFalse(blockade.isValid());
-//    }
-//
-//    @Test
-//    public void testIsValidWithValidInputs() {
-//        List<String> cardList = new ArrayList<>();
-//        d_player.addCard("Blockade");
-//        Blockade blockade = new Blockade(d_player, d_country);
-//        assertTrue(blockade.isValid());
-//    }
-//
-//    @Test
-//    public void testExecute() {
-//        d_player.addCard("Blockade");
-//        Blockade blockade = new Blockade(d_player, d_country);
-//        blockade.execute();
-//        assertEquals(0, d_player.getCountriesOwned().size());
-//        assertEquals(0, d_country.getArmies());
-//    }
-//
-//    @Test
-//    public void testExecuteWithInvalidOrder() {
-//        Blockade blockade = new Blockade(d_player, d_country);
-//        blockade.execute(); // This should not change the state
-//        assertEquals(1, d_player.getCountriesOwned().size());
-//        assertEquals(1, d_country.getArmies());
-//    }
-//
-//    @Test
-//    public void testPrint() {
-//        List<String> cardList = new ArrayList<>();
-//        d_player.addCard("Blockade");
-//        Blockade blockade = new Blockade(d_player, d_country);
-//        blockade.execute();
-//    }
-//}
+package Orders;
+
+import static org.junit.Assert.*;
+
+import Controller.GameEngineController;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+import Models.Country;
+import Models.Player;
+
+public class TestBlockade {
+    private Player d_player;
+    private Country d_country;
+
+    @Before
+    public void setUp() {
+        GameEngineController gameEngineController = new GameEngineController();
+        gameEngineController.executeAddGamePlayer("TestPlayer");
+        d_player = new Player("TestPlayer");
+        gameEngineController.d_Players.add(d_player);
+        d_country = new Country("TestCountry");
+        d_player.addCountryToCountriesOwned(d_country);
+    }
+
+    @Test
+    public void testIsValidWithNullPlayer() {
+        Blockade blockade = new Blockade(null, d_country);
+        assertFalse(blockade.isValid());
+    }
+
+    @Test
+    public void testIsValidWithNullCountry() {
+        Blockade blockade = new Blockade(d_player, null);
+        assertFalse(blockade.isValid());
+    }
+
+    @Test
+    public void testIsValidWithNoBlockadeCard() {
+        Blockade blockade = new Blockade(d_player, d_country);
+        assertFalse(blockade.isValid());
+    }
+
+    @Test
+    public void testIsValidWithValidInputs() {
+        List<String> cardList = new ArrayList<>();
+        d_player.addCard("Blockade");
+        Blockade blockade = new Blockade(d_player, d_country);
+        assertTrue(blockade.isValid());
+    }
+
+    @Test
+    public void testExecute() {
+        d_player.addCard("Blockade");
+        Blockade blockade = new Blockade(d_player, d_country);
+        blockade.execute();
+        assertEquals(0, d_player.getCountriesOwned().size());
+        assertEquals(0, d_country.getArmies());
+    }
+
+    @Test
+    public void testExecuteWithInvalidOrder() {
+        Blockade blockade = new Blockade(d_player, d_country);
+        blockade.execute(); // This should not change the state
+        assertEquals(1, d_player.getCountriesOwned().size());
+        assertEquals(0, d_country.getArmies());
+    }
+
+    @Test
+    public void testPrint() {
+        List<String> cardList = new ArrayList<>();
+        d_player.addCard("Blockade");
+        Blockade blockade = new Blockade(d_player, d_country);
+        blockade.execute();
+        blockade.print();
+    }
+}
--- src/test/java/Orders/TestDiplomacy.java
+++ src/test/java/Orders/TestDiplomacy.java
@@ -2,6 +2,79 @@ package Orders;
 
 import static org.junit.Assert.*;
 
+import Models.Player;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestDiplomacy {
 
+    private Player player1;
+    private Player player2;
+
+    @Before
+    public void setUp() {
+        player1 = new Player("Player 1");
+        player2 = new Player("Player 2");
+    }
+
+    @Test
+    public void testIsValidWithValidConditions() {
+        player1.addCard("Negotiate");
+        Diplomacy diplomacy = new Diplomacy(player1, player2);
+        assertTrue(diplomacy.isValid());
+    }
+
+    @Test
+    public void testIsValidWithoutNegotiateCard() {
+        Diplomacy diplomacy = new Diplomacy(player1, player2);
+        assertFalse(diplomacy.isValid());
+    }
+
+    @Test
+    public void testIsValidWithSelfNegotiation() {
+        player1.addCard("Negotiate");
+        Diplomacy diplomacy = new Diplomacy(player1, player1);
+        assertFalse(diplomacy.isValid());
+    }
+
+    @Test
+    public void testIsValidWithNullPlayer() {
+        player1.addCard("Negotiate");
+        Diplomacy diplomacy = new Diplomacy(player1, null);
+        assertFalse(diplomacy.isValid());
+    }
+
+    @Test
+    public void testIsValidWithNullNegotiatingPlayer() {
+        player1.addCard("Negotiate");
+        Diplomacy diplomacy = new Diplomacy(null, player2);
+        assertFalse(diplomacy.isValid());
+    }
+
+    @Test
+    public void testExecuteWithValidConditions() {
+        player1.addCard("Negotiate");
+        Diplomacy diplomacy = new Diplomacy(player1, player2);
+        diplomacy.execute();
+        assertFalse(player1.getCardList().contains("Negotiate"));
+        assertTrue(player1.getNegotiatePlayers().contains(player2));
+        assertTrue(player2.getNegotiatePlayers().contains(player1));
+    }
+
+    @Test
+    public void testExecuteWithoutNegotiateCard() {
+        Diplomacy diplomacy = new Diplomacy(player1, player2);
+        diplomacy.execute();
+        assertTrue(player1.getCardList().isEmpty());
+        assertTrue(player1.getNegotiatePlayers().isEmpty());
+        assertTrue(player2.getNegotiatePlayers().isEmpty());
+    }
+
+    @Test
+    public void testPrint() {
+        player1.addCard("Negotiate");
+        Diplomacy diplomacy = new Diplomacy(player1, player2);
+        diplomacy.execute();
+        diplomacy.print();
+    }
 }
