ca5d90bc8c061d927353bac253441fa5f52fb8a7
Inderjeet Singh Chauhan <157931131+chauhan9674@users.noreply.github.com>
2024-03-17 23:02:18 -0400

    Update TestBlockade.java

--- src/test/java/Orders/TestBlockade.java
+++ src/test/java/Orders/TestBlockade.java
@@ -9,8 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.List;
+import static org.junit.Assert.*;
 
 /**
  * This class contains unit tests for the Blockade class.
