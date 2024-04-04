a26bc625fb65c2f77be3e6ac5d48bdbe1792e320
Inderjeet Singh Chauhan <inderjeetsinghchauhan1@gmail.com>
2024-04-03 21:13:10 -0400

    Cheater and random strategy

--- src/main/java/Strategy/CheaterStrategy.java
+++ src/main/java/Strategy/CheaterStrategy.java
@@ -3,7 +3,6 @@ package Strategy;
 import Models.Country;
 import Models.Player;
 import Orders.Order;
-
 import java.util.List;
 
 public class CheaterStrategy extends PlayerStrategy{
@@ -13,6 +12,24 @@ public class CheaterStrategy extends PlayerStrategy{
 
     @Override
     public Order createOrder() {
+        for(Country l_cheaterCountry : d_player.getCountriesOwned()){
+            for(Country l_cheaterNeighbour : l_cheaterCountry.getNeighbors().values()){
+                if(l_cheaterNeighbour.getOwner()!=d_player){
+                    Player l_player = l_cheaterNeighbour.getOwner();
+                    l_player.removeCountryFromCountriesOwned(l_cheaterNeighbour);
+                    d_player.addCountryToCountriesOwned(l_cheaterCountry);
+                }
+            }
+        }
+
+        for(Country l_cheaterCountry : d_player.getCountriesOwned()){
+            for(Country l_cheaterNeighbour : l_cheaterCountry.getNeighbors().values()){
+                if(l_cheaterNeighbour.getOwner()!=d_player){
+                    l_cheaterNeighbour.setArmies(l_cheaterNeighbour.getArmies() * 2);
+                    break;
+                }
+            }
+        }
         return null;
     }
 
